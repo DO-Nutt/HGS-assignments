b4e6d07485002d91bcad3df7b1590533b8469729
DO-Nutt <donovan.nutting@hgs.hiddengeniusproject.org>
2016-05-25 19:48:07 -0700

    Add files via upload

---
+++ classroom.py
@@ -0,0 +1,12 @@
+learn = {}
+clss = ""
+grade = ""
+for i in range(0,2):
+    clss = input("What is the class?")
+    grade = input("Enter the corresponding grade")
+    learn[clss] = grade
+print (learn)
+    
+    
+    
+                    
